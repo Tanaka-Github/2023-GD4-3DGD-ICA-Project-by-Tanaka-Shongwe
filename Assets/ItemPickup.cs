9f4b69f04e0b3a5c0402aed25f942ee5890e7eb2
Tanaka-Github <82480720+Tanaka-Github@users.noreply.github.com>
2023-12-29 02:16:42 +0000

    Initial commit

---
+++ Assets/ItemPickup.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Update()
+    {
+        transform.Rotate(0, 9, 0);  
+    }
+
+    // Update is called once per frame
+   private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player") Destroy(gameObject);
+        
+    }
+}
