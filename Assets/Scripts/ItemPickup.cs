bf301322f08bb6e8e0c979aa3a62e176b469a92f
Tanaka-Github <82480720+Tanaka-Github@users.noreply.github.com>
2024-01-02 22:52:05 +0000

    Revert ":w"
    This reverts commit d8f62110dac18346736d6b8e0b43435292133fb7.

--- Assets/Scripts/ItemPickup.cs
+++ Assets/Scripts/ItemPickup.cs
@@ -1,15 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class ItemPickup : MonoBehaviour
 {
-    [SerializeField]
-    public GameObject ScoreText;
-    public int Score = 0;
-
-
     // Start is called before the first frame update
     void Update()
     {
@@ -17,17 +11,12 @@ public class ItemPickup : MonoBehaviour
     }
 
     // Update is called once per frame
-     void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-
-        if (other.tag == "Player")
-        {
-            Score += 50;
-            ScoreText.GetComponent<Text>().text = "SCORE: " + Score;
-            
+        if (other.name == "Player")
+            {
             Debug.Log("Collected");
-            Destroy(gameObject); 
-        }
+            Destroy(gameObject); }
         
     }
 }
