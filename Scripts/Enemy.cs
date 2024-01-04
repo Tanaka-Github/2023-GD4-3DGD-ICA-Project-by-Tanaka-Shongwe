39d265b36f8e8f5d609a431ec73e42546f9bb2fb
Tanaka-Github <82480720+Tanaka-Github@users.noreply.github.com>
2024-01-04 00:16:43 +0000

    :sparkles: added an enemy script

---
+++ Scripts/Enemy.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour {
+
+public float LookRadius = 100f;
+
+NavMeshAgent agent; 
+public Transform Player;
+
+
+	// Use this for initialization
+	void Start () {
+        
+		Player = Playermanager.instance.player.transform;
+		agent = GetComponent<NavMeshAgent>();
+		  
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float distance = Vector3.Distance(Player.position, transform.position);
+
+		if(distance <= LookRadius)
+		{
+		agent.SetDestination(Player.position);			
+			{
+
+				agent.SetDestination(Player.position);
+
+				 if(distance <=agent.stoppingDistance)
+				{
+					//Attack the target
+					 FaceTarget();
+
+				}
+			}
+		}
+        else
+        {
+
+        }
+	}
+
+	void OnDrawGizmosSelected ()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, LookRadius);
+
+
+	}
+
+void FaceTarget()
+{
+  Vector3 direct = ( Player.position - transform.position ) .normalized;
+  Quaternion lookRotation =   Quaternion.LookRotation(new Vector3(direct.x,0, direct.z));
+  transform.rotation = Quaternion.Slerp(transform.rotation ,lookRotation, Time.deltaTime* 100f);
+
+
+
+}
+
+
+	}
+
